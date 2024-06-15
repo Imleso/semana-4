c673324506da43c03fd114824247c3883f9a6dd2
= <=>
2024-06-14 19:55:32 -0500

    semana 4

---
+++ fundamentos/ejemplos.py
@@ -0,0 +1,8 @@
+texto = input("Escribe una palabra: ")
+
+def mayusculasMinuscula(valor):
+    print(valor.upper())
+    print(valor.lower())
+    print(valor.capitalize())
+
+mayusculasMinuscula(texto)
